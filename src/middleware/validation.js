7a4c929518e6185a0a64d4db71f216b2f49c3978
nrizzo64 <nicholas.rizzo64@proton.me>
2025-02-12 19:07:27 -0500

    refactor: update bodyHasExpectedFields middleware function responses to follow JSON:API specification

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,50 +1,48 @@
 const fieldParsers = {
   retailer: (val) => {
-    if (typeof val !== "string")
-      throw new Error(`retailer value is not a string`);
+    if (typeof val !== "string") throw new Error(`value is not a string`);
     // remove non-matching characters
     const sanitized = val.replace(
       /[^\p{L}\p{N}\s'&!@#$%^&*(),.?":{}|<>+-]/gu,
       ""
     );
-    if (sanitized.length < 1)
-      throw new Error(`retailer string must be at least 1 character`);
+    if (sanitized.length < 1) throw new Error(`must be at least 1 character`);
     if (sanitized.length > 100)
-      throw new Error(
-        `retailer string must be less than or equal to 100 characters`
-      );
+      throw new Error(`must be less than or equal to 100 characters`);
     return sanitized;
   },
 
   purchaseDate: (val) => {
     if (!/^\d{4}-\d{2}-\d{2}$/.test(val))
-      throw new Error(`purchaseDate format is invalid. Expecting YYYY-MM-DD`);
+      throw new Error(`format is invalid. Expecting YYYY-MM-DD`);
 
     const date = new Date(`${val}`);
-    if (isNaN(date)) throw new Error(`purchaseDate is not a valid date`);
+    if (isNaN(date)) throw new Error(`not a valid date`);
     return date;
   },
 
   purchaseTime: (val) => {
     if (/^([01]\d|2[0-3]):[0-5]\d$/.test(val)) return val;
-    throw new Error(`purchaseTime is invalid`);
+    throw new Error(`invalid time of day`);
   },
 
   total: (val) => {
     const total = Number(val);
-    if (isNaN(total)) throw new Error(`total is not a valid number`);
+    if (isNaN(total)) throw new Error(`not a valid number`);
     // not handling refunds
-    if (total <= 0) throw new Error(`total should be greater than $0.00`);
+    if (total <= 0) throw new Error(`should be greater than $0.00`);
     return val;
   },
 
   items: (val) => {
-    if (!Array.isArray(val)) throw new Error(`items value should be an array`);
-    if (val.length === 0) throw new Error(`items array should not be empty`);
+    if (!Array.isArray(val)) throw new Error(`should be an array`);
+    if (val.length === 0) throw new Error(`array should not be empty`);
     val.forEach(({ shortDescription, price }, index) => {
       if (!shortDescription)
-        throw new Error(`shortDescription missing from item ${index}`);
-      if (!price) throw new Error(`price missing from item ${index}`);
+        throw new Error(
+          `shortDescription missing from item at index: ${index}`
+        );
+      if (!price) throw new Error(`price missing from item at index: ${index}`);
     });
 
     return val;
@@ -56,12 +54,11 @@ function jsonIsObject(req, res, next) {
     return res.status(400).json({ message: "Request body is not an object" });
   }
 
-  next();
+  return next();
 }
 
 function bodyHasExpectedFields(req, res, next) {
-  let message = "Missing required fields: ";
-  // TODO
+  const { body } = req;
   const requiredFields = [
     "retailer",
     "purchaseDate",
@@ -69,44 +66,47 @@ function bodyHasExpectedFields(req, res, next) {
     "total",
     "items",
   ];
-  const fields = Object.keys(req.body);
-
+  const fields = Object.keys(body);
   const missingFields = requiredFields.filter(
     (field) => !fields.includes(field)
   );
 
-  if (missingFields.length > 0) {
-    message += missingFields.join(", ");
-    return res.status(400).json({ error: message });
+  if (missingFields.length) {
+    const { originalUrl, method} = req;
+    const missingFieldObjects = missingFields.map((mf) => ({
+      title: "Missing required field",
+      detail: `The '${mf}' field is required`,
+      path: originalUrl,
+      method: method,
+      requestBody: body,
+    }));
+    return res.status(400).json({ errors: missingFieldObjects });
   }
 
-  next();
+  return next();
 }
 
 function bodyHasValidFieldValues(req, res, next) {
-  const errors = [];
   // remove quotations from values in json body and compare with
-  let message = "Incorrect values for fields: ";
   const fields = Object.entries(req.body);
-  const parsedFields = fields.map(([key, value]) => {
+  const errors = fields.reduce((acc, [key, value]) => {
     const parser = fieldParsers[key];
     try {
-      return parser(value);
+      parser(value);
+      return acc;
     } catch (error) {
-      errors.push(error);
-      return null;
+      return {
+        ...acc,
+        [key]: error.message,
+      };
     }
-  });
+  }, {});
 
-  errors.forEach((e) => {
-    const splitStack = e.stack.split("\n");
-    console.error(`${splitStack[0]}\n${splitStack[1]}`);
-  });
-  if (errors.length) {
-    return res.status(401).json({ errors: errors.map((e) => e.message) });
+  if (Object.keys(errors).length) {
+    return res.status(401).json({ errors: errors });
   }
 
-  next();
+  return next();
 }
 
 const validateReceipt = [
